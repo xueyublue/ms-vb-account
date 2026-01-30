b74cca3064600a168a054c6a575f58bfaeaf1ec1
Darren <xueyublue@gmail.com>
2026-01-30 22:41:20 +0800

    init

---
+++ src/main/java/sg/darren/ms/vb/account/MsVbAccountApplication.java
@@ -0,0 +1,13 @@
+package sg.darren.ms.vb.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVbAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsVbAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sg/darren/ms/vb/account/MsVbAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.darren.ms.vb.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVbAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
